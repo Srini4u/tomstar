5007664319ecb3b1455b2ba5322dd52f234cae12
Srini <seeni45@gmail.com>
2016-02-11 00:15:33 -0700

    Updated create_docker_run_file.shcker_run_file.sh

--- create_docker_run_file.sh
+++ create_docker_run_file.sh
@@ -3,20 +3,13 @@ DOCKER_TAG=$1
 
 # Dockerrun template
 DOCKERRUN_TEMPLATE=./Dockerrun.aws.json.template
-
-# Prefix of file name is the tag.
+# Name of the new file
 DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
+# AWS S3 bucket path
+S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/"
 
 # Replacing tags in the file and creating a file.
-#sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/"  Dockerrun.aws.json.template > $DOCKERRUN_FILE
-
-
-S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/"
+sed s/<TAG>/$DOCKER_TAG/' -e 's/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/'  Dockerrun.aws.json.template > $DOCKERRUN_FILE
 
 # Uploading json file to $S3_PATH
 aws s3 cp $DOCKERRUN_FILE $S3_PATH
-#aws s3 cp $DOCKERRUN_TEMPLATE $S3_PATH
-
-S3_JSON_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$DOCKERRUN_FILE"
-#sed -e "s/<TAG>/$DOCKER_TAG/" $S3_JSON_PATH
