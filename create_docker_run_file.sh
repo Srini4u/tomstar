467692b904911fb255188de12b926e46910a3cdc
Srini <seeni45@gmail.com>
2016-02-11 00:20:05 -0700

    Updated create_docker_run_file.sh

--- create_docker_run_file.sh
+++ create_docker_run_file.sh
@@ -9,7 +9,7 @@ DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
 S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/"
 
 # Replacing tags in the file and creating a file.
-sed s/<TAG>/$DOCKER_TAG/' -e 's/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/'  Dockerrun.aws.json.template > $DOCKERRUN_FILE
+sed 's/<TAG>/$DOCKER_TAG/' -e 's/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/'  Dockerrun.aws.json.template > $DOCKERRUN_FILE
 
 # Uploading json file to $S3_PATH
 aws s3 cp $DOCKERRUN_FILE $S3_PATH
