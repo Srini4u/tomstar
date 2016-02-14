82e25e871aa0f4d46575b12a9e7f48cbc6f45468
Srini <seeni45@gmail.com>
2016-02-13 20:20:06 -0700

    updated

--- create_docker_run_file.sh
+++ create_docker_run_file.sh
@@ -3,18 +3,17 @@ DOCKER_TAG=$1
 
 # Dockerrun template
 DOCKERRUN_TEMPLATE=./Dockerrun.aws.json.template
+
 # Name of the new file
 DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
+
 # AWS S3 bucket path
 S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/"
 
 # Replacing tags in the file and creating a file.
-#sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" 
-#-e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-
 sed -e 's/<TAG>/$DOCKER_TAG/' -e 's/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/' -e 's/<IMAGE_NAME>/$IMAGE_NAME/' -e 's/<EXPOSED_PORTS>/$EXPOSED_PORTS/' -e 's/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/' < $DOCKERRUN_TEMPLATE > $DOCKERRUN_FILE
 
-echo "DOCKERRUN_FILE=$DOCKERRUN_FILE"
+echo "DOCKERRUN_FILE::=> $DOCKERRUN_FILE"
 
 # Uploading json file to $S3_PATH
 aws s3 cp $DOCKERRUN_FILE $S3_PATH
